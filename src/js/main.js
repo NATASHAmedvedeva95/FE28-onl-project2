53a2322339ba453769d701ef17b82e05c6e3869a
Natalya <natalya-medvedeva95@mail.ru>
2022-04-22 22:22:04 +0300

    :add burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-console.log('cfgjgjk');
+let hamburger =  document.querySelector('.hamburger');
+let navMenu =  document.querySelector('.nav_menu');
+
+
+
+hamburger.addEventListener('click', mobileMenu);
+
+
+function mobileMenu () {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+
+}
