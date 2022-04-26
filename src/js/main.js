08a9baf8cac270e32a024389b1774e0f393eccb1
Natalya <natalya-medvedeva95@mail.ru>
2022-04-27 00:20:05 +0300

    :add slider

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,14 @@ function mobileMenu () {
     navMenu.classList.toggle('active');
     text.classList.toggle('active');
 }
+
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
+		speed:1000,
+		autoplaySpeed:800,
+
+	});
+});
